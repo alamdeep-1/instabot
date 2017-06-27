b6bca6aae643ca852e17e5cdfc43ac354bd4e89b
alamdeep-1 <alamdeep.1@gmail.com>
2017-06-27 11:41:32 +0530

    get the info of a user by username

--- main.py
+++ main.py
@@ -36,6 +36,29 @@ def get_user_id(insta_username):
         exit()
 
 
+def get_user_info(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'There is no data for this user!'
+    else:
+        print 'Status code other than 200 received!'
+
 self_info()
 
 print get_user_id("vivek_shivam")
+
+print get_user_info("vivek_shivam")
+
