32a16b1920fa387f0fa5fd64fcf3f4fe977857cf
alamdeep-1 <alamdeep.1@gmail.com>
2017-06-27 10:47:43 +0530

    searching the user and his id

--- main.py
+++ main.py
@@ -20,4 +20,22 @@ def self_info():
   else:
     print 'Status code other than 200 received!'
 
+
+def get_user_id(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+
 self_info()
+
+print get_user_id("vivek_shivam")
