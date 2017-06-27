b5c451b7bdd6015c764ce313ee71836021faca53
alamdeep-1 <alamdeep.1@gmail.com>
2017-06-27 12:08:59 +0530

    organising all calling functions

--- main.py
+++ main.py
@@ -74,12 +74,54 @@ def get_own_post():
         print 'Status code other than 200 received!'
 
 
+def get_user_post(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
 
-self_info()
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
 
-print get_user_id("vivek_shivam")
 
-print get_user_info("vivek_shivam")
 
-get_own_post()
+def start_bot():
+    while True:
+        print '\n'
+        print 'Hey! Welcome to instaBot!'
+        print 'Here are your menu options:'
+        print "a.Get your own details\n"
+        print "b.Get details of a user by username\n"
+        print "c.Get your own recent post\n"
+        print "d.Get the recent post of a user by username\n"
+        print "j.Exit"
+
+        choice = raw_input("Enter you choice: ")
+        if choice == "a":
+          self_info()
+        elif choice == "b":
+          insta_username = raw_input("Enter the username of the user: ")
+          get_user_info(insta_username)
+        elif choice == "c":
+          get_own_post()
+        elif choice == "d":
+          insta_username = raw_input("Enter the username of the user: ")
+          get_user_post(insta_username)
+        elif choice == "j":
+          exit()
+        else:
+          print "wrong choice"
+
+start_bot()
 
